6de6c77cba82d88c0527c81ffa2cfd9c632dae31
sawwas08 <sawwas08@icloud.com>
2026-09-14 20:20:17 -0400

    testpy

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# myscript.py
-
-def main():
-    print("This is the main function executing!")
-
-if __name__ == "__main__":
-    # This block only runs if you do: python myscript.py
-    main()
+print("This is the main function executing!")
