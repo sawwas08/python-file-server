188a4090c9f8cd3241d2a7f58c6d984685131391
sawwas08 <sawwas08@icloud.com>
2026-09-15 15:23:06 -0400

    test gitignore

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("This is the main function executing!")
+from pathlib import Path
+
+
+dir_path = Path("../data/")
+dir_path.mkdir(parents=True, exist_ok=True)
+
+def main():
+    print("MAIN CALLED")
+    retrieveEnv()
+    initFileSystem()
+    return
+
+def retrieveEnv():
+    print("retrieving env")
+    return
+
+def initFileSystem():
+    print("initializing filesystem")
+    return
+
+#python is weird and doesnt automatically call entry point, so main gets called here after everything is defined
+main()
