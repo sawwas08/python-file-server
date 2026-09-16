196d4feecefcd071e2e2a320d8a88407d1729623
sawwas08 <sawwas08@icloud.com>
2026-09-16 19:21:59 -0400

    added drives.py

--- scripts/init-db.py
+++ scripts/init-db.py
@@ -7,7 +7,5 @@ current_dir = Path(__file__).resolve().parent; root_dir = current_dir.parent
 if str(root_dir) not in sys.path:
     sys.path.insert(0, str(root_dir))
 
-######
 from src.controllers import sqlite
-
 sqlite.initDbFiles()
